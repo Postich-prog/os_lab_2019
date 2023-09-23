1e203db3c6492d3f059a157755339bfe6751d989
Postich-prog <97094618+Postich-prog@users.noreply.github.com>
2023-09-23 10:57:47 +0000

    1 2 выполнены

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,24 @@
 #include "revert_string.h"
+#include <stdlib.h>
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    if (str == NULL) { 
+        return; 
+    } 
+  
+    // получаем указатель на конец последнего символа в строке 
+    char* end_ptr = str + (strlen(str) - 1); 
+  
+    // начинаем менять местами символы с обоих концов строки 
+    while (end_ptr > str) 
+    { 
+        char ch = *str; 
+        *str = *end_ptr; 
+        *end_ptr = ch; 
+  
+        // увеличить строку и уменьшить end_ptr 
+        ++str, --end_ptr; 
+    }
 }
-
